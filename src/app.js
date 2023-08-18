9132a1b62ba2ae04f1522a622d8a36051c04857f
Sami Khan <sami999khan999@gmail.com>
2023-08-18 23:52:33 +0600

    done

--- src/app.js
+++ src/app.js
@@ -1,4 +1,9 @@
 const productWrapper = document.querySelector(".product-wrapper");
+const addedProduct = document.querySelector(".added-products");
+const modalClose = document.querySelector(".modal-close");
+const modal = document.querySelector(".modal");
+const cart = document.querySelector(".cart");
+const itemCount = document.querySelector(".item-count");
 
 //=================================================================================================================//
 // fetching product
@@ -11,7 +16,10 @@ function getProducts() {
         throw new Error("Products not found, please try again later!");
       return res.json();
     })
-    .then((data) => renderProducts(data))
+    .then((data) => {
+      renderProducts(data);
+      renderLocalData(data);
+    })
     .catch((err) => renderError(err));
 }
 
@@ -81,9 +89,35 @@ function renderProducts(products) {
   addToCart.forEach((btn) => {
     btn.addEventListener("click", function (e) {
       const id = e.target.dataset.id;
-      fetchingSingleProduct(id);
+
+      // cart open
+      modal.classList.add("-translate-x-0");
+      modal.classList.remove("translate-x-full");
+
+      // checking local data
+      const checkingLocalData = JSON.parse(localStorage.getItem(`item-${id}`));
+
+      if (checkingLocalData) {
+        return null;
+      }
+
+      if (!checkingLocalData) {
+        // rumber of items in cart
+        numberOfItems();
+        // calling another fetch function
+        fetchingSingleProduct(id);
+      }
     });
   });
+}
+
+//=================================================================================================================//
+// number of items in cart
+//=================================================================================================================//
+
+function numberOfItems() {
+  const items = Number(itemCount.textContent) + 1;
+  itemCount.textContent = items;
 }
 
 //=================================================================================================================//
@@ -93,14 +127,111 @@ function renderProducts(products) {
 function fetchingSingleProduct(id) {
   fetch(`http://localhost:8000/products/${id}`)
     .then((res) => res.json())
-    .then((data) => renderSingleProduct(data));
+    .then((data) => {
+      saveInLocalStroage(data);
+      renderSingleProduct(data);
+    });
 }
 
 //=================================================================================================================//
-// render single product
+// save in local stroage
 //=================================================================================================================//
 
-function renderSingleProduct(product) {}
+function saveInLocalStroage(product) {
+  // getting data from locel stroage
+  const getLocalData = JSON.parse(localStorage.getItem(`item-${product.id}`));
+
+  // if data exists return null
+  if (getLocalData) return null;
+
+  // if data does not exists
+  if (!getLocalData) {
+    localStorage.setItem(`item-${product.id}`, JSON.stringify(product));
+  }
+}
+
+//=================================================================================================================//
+// rendering local data
+//=================================================================================================================//
+
+function renderLocalData(products) {
+  let localData = [];
+
+  for (let i = 1; i <= products.length; i++) {
+    const dataParsing = JSON.parse(localStorage.getItem(`item-${i}`));
+    if (dataParsing) localData.push(dataParsing);
+  }
+
+  // render local data
+  localData.forEach((product) => {
+    renderSingleProduct(product);
+  });
+
+  // render item count
+  const items = localData.length;
+  itemCount.textContent = items;
+}
+
+//=================================================================================================================//
+// rendering single product
+//=================================================================================================================//
+
+function renderSingleProduct(product) {
+  const html = `
+  <div
+  class="added-product grid grid-cols-4 border-b pb-2 justify-center overflow-hidden gap-2 items-center "
+>
+  <div
+    class="img overflow-hidden w-20 rounded flex justify-center items-center cursor-pointer"
+  >
+    <img
+      src="${product.image}"
+      alt="${product.title}"
+      class="block w-full rounded"
+    />
+  </div>
+  <div class="texts flex flex-col gap-2 col-span-2">
+    <h4 class="font-semibold">${product.title}</h4>
+    <div class="flex justify-between items-center">
+      <p class="price font-bold text-rose-500">$${currencyFormate(
+        product.price
+      )}</p>
+      <p
+        class="font-semibold text-xl overfolw-hidden flex items-center bg-purple-100 gap-3"
+      >
+        <span
+          class="bg-violet-500 text-violet-50 px-3 cursor-pointer active:bg-rose-500 select-none"
+          >-</span
+        ><span>1</span
+        ><span
+          class="bg-violet-500 text-violet-50 px-3 cursor-pointer active:bg-rose-500 select-none"
+          >+</span
+        >
+      </p>
+    </div>
+  </div>
+  <button
+    class="romove-item hover:text-rose-500 justify-self-end pt-10 duration-200"
+  >
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke-width="1.5"
+      stroke="currentColor"
+      class="w-6 h-6"
+    >
+      <path
+        stroke-linecap="round"
+        stroke-linejoin="round"
+        d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
+      />
+    </svg>
+  </button>
+</div>
+  `;
+  addedProduct.insertAdjacentHTML("afterbegin", html);
+}
 
 //=================================================================================================================//
 // rendering error
@@ -113,3 +244,21 @@ function renderError(err) {
 
   productWrapper.insertAdjacentHTML("afterbegin", html);
 }
+
+//=================================================================================================================//
+// modal open
+//=================================================================================================================//
+
+cart.addEventListener("click", function () {
+  modal.classList.add("-translate-x-0");
+  modal.classList.remove("translate-x-full");
+});
+
+//=================================================================================================================//
+// modal close
+//=================================================================================================================//
+
+modalClose.addEventListener("click", function () {
+  modal.classList.add("translate-x-full");
+  modal.classList.remove("-translate-x-0");
+});
